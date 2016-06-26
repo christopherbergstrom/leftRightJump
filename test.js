c9d3fa5e3f8112adf2bceb1890072af432e018e2
Christopher Bergstrom <christopherrbergstrom@gmail.com>
2016-06-26 17:08:06 -0600

    init commit

---
+++ test.js
@@ -0,0 +1,114 @@
+var keysDown = [];
+var intArray = [];
+$(document).ready(function()
+{
+  var down = true;
+  var jump = true;
+  var hor = 0;
+  var vert = window.innerHeight/10*7;
+  var int;
+  var block = $("#block");
+  block.css("marginTop", vert+"px");
+  $(document).keydown(function(e)
+  {
+    keysDown[e.which] = true;
+    console.log(down);
+    // right
+    if (keysDown[39] && down)
+    {
+      console.log("right");
+      console.log(down);
+      down = false;
+      int = setInterval(function()
+      {
+        hor += 1;
+        block.css("marginLeft", hor+"px");
+      }, 1);
+      intArray.push(int);
+    }
+    // left
+    if (keysDown[37] && down)
+    {
+      console.log("right");
+      console.log(down);
+      down = false;
+      int = setInterval(function()
+      {
+        hor -= 1;
+        block.css("marginLeft", hor+"px");
+      }, 1);
+      intArray.push(int);
+    }
+    // // up
+    // if (keysDown[38] && down)
+    // {
+    //   down = false;
+    //   int = setInterval(function()
+    //   {
+    //     vert -= 1;
+    //     block.css("marginTop", vert+"px");
+    //   }, 1);
+    // }
+
+    // jump
+    if (keysDown[38] && jump)
+    {
+      console.log("jump");
+      jump = false;
+      var upInterval = setInterval(function()
+      {
+        vert -= 2;
+        block.css("marginTop", vert+"px");
+      }, 1);
+      var upTimeout = setTimeout(function()
+      {
+        clearInterval(upInterval);
+        var downInterval = setInterval(function()
+        {
+          vert += 2;
+          block.css("marginTop", vert+"px");
+        }, 1);
+        var downTimeout = setTimeout(function()
+        {
+          clearInterval(downInterval);
+          jump = true;
+          console.log(jump);
+        }, 300);
+      }, 300);
+    }
+
+
+    // jump right
+    if (keysDown[39] && down && keysDown[38] && jump)
+    {
+      console.log("right jump");
+      // down = false;
+      // int = setInterval(function()
+      // {
+        vert -= 1;
+        hor += 1;
+        $("#block").css("marginTop", vert+"px");
+        $("#block").css("marginLeft", hor+"px");
+      // }, 1);
+    }
+  }).keyup(function(e)
+  {
+    keysDown[e.which] = false;
+    // for (var i = 0; i < intArray.length; i ++)
+    // {
+    //   clearInterval(intArray[i]);
+    // }
+    // intArray = [];
+    // down = true;
+    if (int)
+    {
+      console.log("int here");
+      clearInterval(int);
+      down = true;
+    }
+    else
+    {
+      console.log("int NOT here");
+    }
+  });
+});
